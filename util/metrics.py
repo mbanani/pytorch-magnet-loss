51233d2212a4ee1e8f0c5d22b9bd27c3a8c3c40c
Mohamed El Banani <mbanani@umich.edu>
2018-02-12 15:14:42 -0500

    moved old metrics

--- util/metrics.py
+++ util/metrics.py
@@ -1,39 +1,198 @@
-import numpy as np
-import scipy.misc
+"""
+    A set of evaluation metrics for clustering
 
-from IPython import embed
+    NMI, Purity and RI are calculated using equations in the following link:
+    https://nlp.stanford.edu/IR-book/html/htmledition/evaluation-of-clustering-1.html
 
-class metrics(object):
+    Hungarian Algorithm obtained from https://github.com/tdedecko/hungarian-algorithm
 
-    def __init__(self):
+"""
+
+import numpy    as np
+
+from sklearn    import cluster
+from .hungarian import Hungarian
+from IPython    import embed
+
+def confusion_matrix_st(preds, labels):
+        num_inst   = len(preds)
+        num_labels = np.max(labels) + 1
+
+        conf_matrix = np.zeros( (num_labels, num_labels))
+
+        for i in range(0, num_inst):
+            gt_i = labels[i]
+            pr_i = preds[i]
+            conf_matrix[gt_i, pr_i] = conf_matrix[gt_i, pr_i] + 1
+
+        return conf_matrix
+
+def confusion_matrix(preds, labels):
+        num_inst   = preds.shape[0]
+        num_labels = preds.shape[1]
+        preds      = np.argmax(preds, axis=1)
+
+        conf_matrix = np.zeros( (num_labels, num_labels))
+
+        for i in range(0, num_inst):
+            gt_i = labels[i]
+            pr_i = preds[i]
+            conf_matrix[gt_i, pr_i] = conf_matrix[gt_i, pr_i] + 1
+
+        return conf_matrix
+
+def correct_confusion_matrix(conf_matrix):
+    num_labels = conf_matrix.shape[0]
+    new_conf = np.zeros( (num_labels, num_labels) )
+    hungarian = Hungarian(conf_matrix, is_profit_matrix = True)
+    hungarian.calculate()
+    results = hungarian.get_results()
+    correct = np.zeros(num_labels)
+    for i in range(0, num_labels):
+        for j in range(0, num_labels):
+            new_conf[results[i][0], results[j][0]] = conf_matrix[results[i][0], results[j][1]]
+    print new_conf
+    return new_conf
+
+def get_hungarian_results(conf_matrix):
+    hungarian = Hungarian(conf_matrix, is_profit_matrix = True)
+    hungarian.calculate()
+    results = hungarian.get_results()
+    return results
+
+def calculate_accuracy(conf_matrix):
+    num_labels = conf_matrix.shape[0]
+    hungarian = Hungarian(conf_matrix, is_profit_matrix = True)
+    hungarian.calculate()
+    results = hungarian.get_results()
+
+    correct = np.zeros(num_labels)
+    for i in range(0, num_labels):
+        correct[results[i][0]] = conf_matrix[results[i][0], results[i][1]]
+
+    overall_accuracy = np.sum(correct) / np.sum(conf_matrix)
+    return overall_accuracy, correct
+
+def calculate_purity(conf_matrix):
+    # purity = 1/N * sum_{c = clusters} max_{t= classes} (union of cluster_t and class_t)
+    num_inst    = np.sum(conf_matrix).astype(float)
+    best_unions = np.amax(conf_matrix, axis = 0)
+    return np.sum(best_unions) / num_inst
+
+def calculate_NMI(conf_matrix):
+    num_labels = conf_matrix.shape[0]
+    _min_float = 1e-12
+
+    conf_matrix = conf_matrix.astype(float)
+    total_inst  = np.sum(conf_matrix)
+
+    prob_matrix = conf_matrix / total_inst
+    P_gt        = np.sum(prob_matrix, axis=1)
+    P_pred      = np.sum(prob_matrix, axis=0)
+
+    # Clip for numerical stability
+    prob_matrix = np.clip(prob_matrix, a_min = _min_float, a_max = None)
+    P_gt        = np.clip(P_gt, a_min = _min_float, a_max = None)
+    P_pred      = np.clip(P_pred, a_min = _min_float, a_max = None)
 
 
-        self.results_pred   = []
-        self.results_label  = []
+    mutual_info = 0.0
 
+    for i in range(0, num_labels):
+        for j in range(0, num_labels):
+            mutual_info += prob_matrix[i,j] * np.log(prob_matrix[i,j] / (P_gt[i] * P_pred[j]))
 
-    """
-        Updates the keypoint dictionary
-        params:     obj_class       object class                    (batch_size)
-                    predictions     predictions for each image      (batch_size x 360*NumClasses)
-                    labels          labels for each image           (batch_size x 360*NumClasses)
-    """
-    def update_dict(self, predictions, labels):
-        """Log a scalar variable."""
-        if type(predictions) == int:
-            predictions = [predictions]
-        if type(labels) == int:
-            labels = [labels]
+    entropy_gt   = -1. * np.sum(P_gt * np.log(P_gt))
+    entropy_pred = -1. * np.sum(P_pred * np.log(P_pred))
 
+    NMI = 2. * mutual_info / (entropy_pred + entropy_gt)
 
-        for i in range(0, len(obj_class)):
-            start_index = self.class_ranges[obj_class[i]]
-            self.results_pred.append( [ np.argmax(predictions[0][i, start_index:start_index+360]),
-                                        np.argmax(predictions[1][i, start_index:start_index+360]),
-                                        np.argmax(predictions[2][i, start_index:start_index+360])])
-            self.results_label.append( [ np.argmax(labels[0][i, start_index:start_index+360]),
-                                         np.argmax(labels[1][i, start_index:start_index+360]),
-                                         np.argmax(labels[2][i, start_index:start_index+360])])
+    return NMI
 
-    def metrics(self):
-        pass
+def calculate_spectral_clustering_accuracy(np_activations, np_labels, num_clusters):
+
+    activations = np_activations[0]
+    labels      = np_labels[0]
+
+    # Compute spectral clustering accuracy
+    for i in range(1, len(np_activations)):
+        activations = np.concatenate((activations, np_activations[i]), axis=0)
+        labels      = np.concatenate((labels, np_labels[i]), axis=0)
+
+    # END FOR
+
+    # Setup of spectral clustering algorithm
+    algorithm = cluster.SpectralClustering(n_clusters=num_clusters)
+
+    # Obtain predictions from spectral clustering model
+    predictions = algorithm.fit_predict(activations)
+
+    # Compute confusion matrix from predictions
+    conf_matrix = confusion_matrix_st(predictions, labels)
+
+    # Use hungarian algorithm to compare cluster labels vs. true class labels
+    hungarian = Hungarian(conf_matrix, is_profit_matrix = True)
+    hungarian.calculate()
+    results = hungarian.get_results()
+
+    correct = np.zeros(num_clusters)
+
+    for i in range(0, num_clusters):
+        correct[results[i][0]] = conf_matrix[results[i][0], results[i][1]]
+
+    # END FOR
+
+    overall_accuracy = np.sum(correct) / np.sum(conf_matrix)
+
+    return overall_accuracy
+
+def calculate_kmeanspp_clustering_accuracy(np_activations, np_labels, num_clusters):
+
+    activations = np_activations[0]
+    labels      = np_labels[0]
+
+    # Compute spectral clustering accuracy
+    for i in range(1, len(np_activations)):
+        activations = np.concatenate((activations, np_activations[i]), axis=0)
+        labels      = np.concatenate((labels, np_labels[i]), axis=0)
+
+    # END FOR
+
+    # Setup of kmeans++ clustering algorithm
+    algorithm = cluster.KMeans(n_clusters=num_clusters, init='k-means++')
+
+    # Obtain predictions from spectral clustering model
+    predictions = algorithm.fit_predict(activations)
+
+    # Compute confusion matrix from predictions
+    conf_matrix = confusion_matrix_st(predictions, labels)
+
+    # Use hungarian algorithm to compare cluster labels vs. true class labels
+    hungarian = Hungarian(conf_matrix, is_profit_matrix = True)
+    hungarian.calculate()
+    results = hungarian.get_results()
+
+    correct = np.zeros(num_clusters)
+
+    for i in range(0, num_clusters):
+        correct[results[i][0]] = conf_matrix[results[i][0], results[i][1]]
+
+    # END FOR
+
+    overall_accuracy = np.sum(correct) / np.sum(conf_matrix)
+
+    return overall_accuracy
+
+def cluster_metrics(conf_matrix, np_activations, np_labels):
+    accuracy, _     = calculate_accuracy(conf_matrix)
+
+    return accuracy
+
+def cluster_metrics_eval(conf_matrix, np_activations, np_labels):
+    NMI             = calculate_NMI(conf_matrix)
+    purity          = calculate_purity(conf_matrix)
+    accuracy, _     = calculate_accuracy(conf_matrix)
+    s_accuracy      = calculate_spectral_clustering_accuracy(np_activations, np_labels, conf_matrix.shape[0])
+    k_accuracy      = calculate_kmeanspp_clustering_accuracy(np_activations, np_labels, conf_matrix.shape[0])
+
+    return accuracy, purity, NMI, s_accuracy, k_accuracy
---
+++ util/tSNE.py
@@ -0,0 +1,23 @@
+import numpy                as numpy
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot    as plt
+
+from sklearn.manifold   import TSNE
+import time
+
+
+def tSNE_visual(activations, labels, show=False, save_path=None):
+    print "Calculating tSNE for array of size: ", activations.shape
+    t_start = time.time()
+    tsne = TSNE()
+    active_tsne = tsne.fit_transform(activations)
+    elapsed_time = time.time() - t_start
+    print "tSNE calculated! time elapsed: %.3f seconds" % elapsed_time
+    fig = plt.figure(1, (10, 10))
+    plt.scatter(active_tsne[:, 0], active_tsne[:, 1], c=labels)
+
+    if show:
+        fig.show()
+    if save_path:
+        fig.savefig(save_path)
